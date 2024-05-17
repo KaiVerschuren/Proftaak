ece1e8527c09ace65f4990f279a8afcbb5ca0722
Kai Verschuren <Verschurenkai@gmail.com>
2024-05-17 10:27:14 +0200

    head function

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php 
 include("inc/php/functions.php");
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage | Coin Cove</title>
-</head>
+head();
+?>
 
 <body>
-    <?php
-    test();
-        ?>
+
 </body>
 
 </html>
