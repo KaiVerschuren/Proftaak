26b3bf3e2a3ba92b8105fc8e18fc67804e1ffc3f
Kai Verschuren <Verschurenkai@gmail.com>
2024-05-21 14:12:28 +0200

    leaderboard

--- index.php
+++ index.php
@@ -25,6 +25,9 @@ head();
                 <h2 class="userCounterTitle">Trusted by <span class="userCounterAmount">999,999,999</span> users!</h2>
             </div>
         </section>
+        <section>
+            
+        </section>
     </main>
 
 </body>
