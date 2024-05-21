6a5b8368a05308d9e564e7cd5f8fcc3f4b6e18b0
Kai Verschuren <Verschurenkai@gmail.com>
2024-05-21 12:05:26 +0200

    made hero | finishing touches to header

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
-<?php 
+<?php
 include("inc/php/functions.php");
 
 head();
 ?>
 
 <body>
-<script defer src="inc/js/main.js"></script>
+    <script defer src="inc/js/main.js"></script>
     <?php HeaderFunction(); ?>
-
+    <main class="container">
+        <section class="hero">
+            <div class="heroTextWrapper">
+                <div>
+                    <h1 class="heroTitle">Coin Cove</h1>
+                </div>
+                <div class="heroParagraph">
+                    <strong>Buy, sell, and store</strong> over 250 digital assets at one of Europe’s<strong> leading exchanges</strong>.
+                </div>
+                <button class="heroButton btn">Start Trading!</button>
+            </div>
+            <img class="heroImage" src="./assets/Placeholder1000x800.jpg" alt="">
+        </section>
+    </main>
 
 </body>
 
