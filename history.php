2b8418f66f45a9d4ef445563feb9fade32f2d25c
LucasKnol <91659@roc-teraa.nl>
2024-05-30 09:32:52 +0200

    History
    Added descriptions. Made it so you are able to scroll to the next project with information.

--- history.php
+++ history.php
@@ -14,12 +14,36 @@ head("Homepage");
 <body>
     <?php mobileNav(); ?>
     <?php HeaderFunction(); ?>
-        <div class = "calculator">
-            <img class= "calculator_photo" src= "./assets/Calculator.png" alt="A photo off the calculator. Our first object">
-                <div class = "calculator_description">
-                    This was  our first project we had to make. Its a basic calculator
-                </div>
+
+    <div class="slideshow-container">
+    <div class="projectGrid">
+        <div class="project">
+            <img class="photo" src="./assets/Calculator.png" alt="A photo of the calculator, our first project">
+            <div class="description">
+                This was our very first project. In this project we learned how to use Css, php and html. 
+                With the information we got from the lessons we had to make a calculator that can multiply, divide, subtract, add, square root, 
+                <br><br> Hold shift and scroll down to go the next page.
+            </div>
         </div>
+        <div class="project">
+            <img class="photo" src="./assets/Radio.png" alt="A photo of the radio, our second project">
+            <div class="description">
+                This was our second project. In this project we learned how to insert and use a database.
+                With this information we had to make a basic radio where the user will see three artist and when they click on a artist 3 random songs will show up.
+                <br><br> hold shift and scroll down to go to the next page and hold shiftand scroll up to go back
+            </div>
+        </div>
+        <div class="project">
+            <img class="photo" src="./assets/Shop.png" alt="A photo of the shop, our third project">
+            <div class="description">
+                This was our third project. In this project we learned how to make a webshop using php, html, css and a database.
+                In the database we added pictures, prices, descriptions, item name, item id and a filter bar.
+                <br><br> hold shift and scroll up to go back
+            </div>
+        </div>
+    </div>
+</div>
+
 
     <?php footer(); ?>
     
